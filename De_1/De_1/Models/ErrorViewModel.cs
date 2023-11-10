8101208488e4dc226e30d2628fcefcbe070b1999
MTQ05032022 <maituquy1293@gmail.com>
2023-11-10 13:41:37 +0700

    init project

---
+++ De_1/De_1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace De_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
